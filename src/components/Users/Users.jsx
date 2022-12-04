fe2802d36d03618006cf3067720ea8421a95537b
m-diana-dev <m.diana.dev@gmail.com>
2022-12-04 16:53:36 +0300

    users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,17 +1,14 @@
 // import React from "react";
-import classes from './Users.module.css'
+import classes from './Users.module.css';
+import axios from "axios";
+import userAvatar from '../../assets/images/avatar.jpg'
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                { id: 1, followed: true, img: 'https://www.shutterstock.com/image-vector/cartoon-character-jack-russell-terrier-260nw-579387331.jpg', fullName: 'Bobik', status: 'Uf, uf', location: { country: 'Russia', city: 'Moscow' } },
-                { id: 2, followed: true, img: 'https://cdn1.vectorstock.com/i/1000x1000/08/75/cute-yorkshire-terrier-dog-avatar-vector-20670875.jpg', fullName: 'Chappy', status: 'Hello', location: { country: 'Russia', city: 'Omsk' } },
-                { id: 3, followed: false, img: 'https://cdn4.iconfinder.com/data/icons/halloween-avatar-flat/64/halloween-33-512.png', fullName: 'Kesha', status: '...', location: { country: 'Russia', city: 'Moscow' } },
-                { id: 4, followed: false, img: 'https://cdn3.vectorstock.com/i/1000x1000/15/32/isolated-cute-cat-avatar-vector-21041532.jpg', fullName: 'Murka', status: 'I am a cat', location: { country: 'Russia', city: 'Smolensk' } },
-                { id: 5, followed: true, img: 'https://thumbs.dreamstime.com/b/cute-pug-dog-avatar-cute-pug-dog-avatar-vector-illustration-design-114995824.jpg', fullName: 'Bobik', status: 'ZzzzZzz...', location: { country: 'Russia', city: 'Penza' } },
-            ]
-        );
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            // debugger;
+            props.setUsers(response.data.items);
+        })
     }
     return (
         <div className={classes.users}>
@@ -21,10 +18,10 @@ let Users = (props) => {
                     props.users.map(user =>
                         <div key={user.id} className={classes.userItem}>
                             <div className={classes.userItemInner}>
-                                <img src={user.img} />
+                                <img src={user.photos.small != null ? user.photos.small : userAvatar} />
                                 <div className={classes.userItemWrapp}>
-                                    <a href="#" className={classes.userItemName}>{user.fullName}</a>
-                                    <div className={classes.userItemLocation}><span>{user.location.country}</span><span>{user.location.city}</span></div>
+                                    <a href="#" className={classes.userItemName}>{user.name}</a>
+                                    <div className={classes.userItemLocation}><span>{"user.location.country"}</span><span>{"user.location.city"}</span></div>
                                     <div className={classes.userItemStatus}>{user.status}</div>
                                 </div>
                             </div>
