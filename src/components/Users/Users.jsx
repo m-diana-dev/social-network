6f432d594c386b35c0324f7b1216275af8abf051
m-diana-dev <m.diana.dev@gmail.com>
2022-12-05 12:06:33 +0300

    replacing a functional component with a class component (53)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,43 +1,46 @@
-// import React from "react";
+import React from "react";
 import classes from './Users.module.css';
 import axios from "axios";
 import userAvatar from '../../assets/images/avatar.jpg'
 
-let Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                // debugger;
-                props.setUsers(response.data.items);
-            })
-        }
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        alert("new");
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            // debugger;
+            this.props.setUsers(response.data.items);
+        })
     }
-    return (
-        <div className={classes.users}>
-            <h1>Users</h1>
-            <button onClick={getUsers} className={classes.usersBtn}>Get Users</button>
-            <div className={classes.usersItems}>
-                {
-                    props.users.map(user =>
-                        <div key={user.id} className={classes.userItem}>
-                            <div className={classes.userItemInner}>
-                                <img src={user.photos.small != null ? user.photos.small : userAvatar} />
-                                <div className={classes.userItemWrapp}>
-                                    <a href="#" className={classes.userItemName}>{user.name}</a>
-                                    <div className={classes.userItemLocation}><span>{"user.location.country"}</span><span>{"user.location.city"}</span></div>
-                                    <div className={classes.userItemStatus}>{user.status}</div>
+
+
+    render() {
+        return (
+            <div className={classes.users}>
+                <h1>Users</h1>
+                <div className={classes.usersItems}>
+                    {
+                        this.props.users.map(user =>
+                            <div key={user.id} className={classes.userItem}>
+                                <div className={classes.userItemInner}>
+                                    <img src={user.photos.small != null ? user.photos.small : userAvatar} />
+                                    <div className={classes.userItemWrapp}>
+                                        <a href="#" className={classes.userItemName}>{user.name}</a>
+                                        <div className={classes.userItemLocation}><span>{"user.location.country"}</span><span>{"user.location.city"}</span></div>
+                                        <div className={classes.userItemStatus}>{user.status}</div>
+                                    </div>
                                 </div>
+                                {user.followed
+                                    ? <button onClick={() => { this.props.unFollow(user.id) }} className={classes.unfollow}>Unfollow</button>
+                                    : <button onClick={() => { this.props.follow(user.id) }} className={classes.follow}>Follow</button>
+                                }
                             </div>
-                            {user.followed
-                                ? <button onClick={() => { props.unFollow(user.id) }} className={classes.unfollow}>Unfollow</button>
-                                : <button onClick={() => { props.follow(user.id) }} className={classes.follow}>Follow</button>
-                            }
-                        </div>
-                    )
-                }
+                        )
+                    }
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Users;
